c763f52c22c749b8df13c86f4d75fbeae34a795b
Lawfer <Lawfer@gmail.com>
2018-01-28 22:16:42 -0500

    Added exercise 2.8

---
+++ Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Why is 6 afraid of 7?"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Because 7 ate 9!"
+    End Sub
+
+End Class
