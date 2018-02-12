8ed4378a0b4922c5a9b0ed1082b98ba7026d07bd
Lawfer <Lawfer@gmail.com>
2018-02-12 14:13:31 -0500

    Added Stadium Ticket Assignment

---
+++ Lab4StadiumSeating/Lab4StadiumSeating/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblA = New System.Windows.Forms.Label()
+        Me.lblB = New System.Windows.Forms.Label()
+        Me.lblC = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtA)
+        Me.GroupBox1.Controls.Add(Me.txtB)
+        Me.GroupBox1.Controls.Add(Me.txtC)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(252, 177)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tickets Sold"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 134)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Class C"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Class B"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Class A"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(33, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the number of tickets sold for each class of seats."
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotal)
+        Me.GroupBox2.Controls.Add(Me.lblC)
+        Me.GroupBox2.Controls.Add(Me.lblB)
+        Me.GroupBox2.Controls.Add(Me.lblA)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(292, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(251, 177)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Revenue Generated"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.AutoSize = True
+        Me.btnCalc.Location = New System.Drawing.Point(65, 227)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(108, 23)
+        Me.btnCalc.TabIndex = 1
+        Me.btnCalc.Text = "Calculate Revenue"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(240, 227)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(389, 227)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(96, 65)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(65, 20)
+        Me.txtA.TabIndex = 4
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(96, 99)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(65, 20)
+        Me.txtB.TabIndex = 5
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(96, 131)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(65, 20)
+        Me.txtC.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(19, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Class A"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(19, 65)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Class B"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(19, 106)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Class C"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(19, 148)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 13)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Total Revenue"
+        '
+        'lblA
+        '
+        Me.lblA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblA.Location = New System.Drawing.Point(127, 28)
+        Me.lblA.Name = "lblA"
+        Me.lblA.Size = New System.Drawing.Size(100, 23)
+        Me.lblA.TabIndex = 4
+        '
+        'lblB
+        '
+        Me.lblB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblB.Location = New System.Drawing.Point(127, 65)
+        Me.lblB.Name = "lblB"
+        Me.lblB.Size = New System.Drawing.Size(100, 23)
+        Me.lblB.TabIndex = 5
+        '
+        'lblC
+        '
+        Me.lblC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblC.Location = New System.Drawing.Point(127, 101)
+        Me.lblC.Name = "lblC"
+        Me.lblC.Size = New System.Drawing.Size(100, 23)
+        Me.lblC.TabIndex = 6
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(127, 147)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents txtB As TextBox
+    Friend WithEvents txtC As TextBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblC As Label
+    Friend WithEvents lblB As Label
+    Friend WithEvents lblA As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Lab4StadiumSeating/Lab4StadiumSeating/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Const A As Decimal = 15
+        Const B As Decimal = 12
+        Const C As Decimal = 9
+        Dim ATicket As Decimal
+        Dim BTicket As Decimal
+        Dim CTicket As Decimal
+        Dim ARevenue As Decimal
+        Dim BRevenue As Decimal
+        Dim CRevenue As Decimal
+
+        Try
+            ATicket = CDec(txtA.Text)
+            BTicket = CDec(txtB.Text)
+            CTicket = CDec(txtC.Text)
+            ARevenue = A * ATicket
+            BRevenue = B * BTicket
+            CRevenue = C * CTicket
+
+            lblA.Text = A * ATicket
+            lblB.Text = B * BTicket
+            lblC.Text = C * CTicket
+            lblTotal.Text = ARevenue + BRevenue + CRevenue
+        Catch ex As Exception
+            MessageBox.Show("Please make sure no fields are left blank.")
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtA.Clear()
+        txtB.Clear()
+        txtC.Clear()
+
+        lblA.Text = ""
+        lblB.Text = ""
+        lblC.Text = ""
+        lblTotal.Text = ""
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
